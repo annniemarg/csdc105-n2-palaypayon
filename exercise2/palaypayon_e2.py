aa015c11600b2a72a5b9b09107e51614551ba43d
Anne Margaret <69958245+annniemarg@users.noreply.github.com>
2020-09-09 06:16:25 +0800

    Create palaypayon_e2.py

---
+++ exercise2/palaypayon_e2.py
@@ -0,0 +1,17 @@
+# Author          : Anne Margaret Palaypayon
+   # Course and Year : BSIT-2
+   # Filename        :  Palaypayon_e2.py
+   # Description     : Print the following lines containing info about yourself:
+   # Honor Code      : I have not given nor received any unathorized help in
+   #                   completing this exercise. I am also well aware of the 
+   #                   policies stipulated in the AdNU student handbook.
+
+
+print("I am Anne Margaret F. Palaypayon.")
+print("My spirit animal is wolf.")
+print("because I know some of its characteristics define who I am, like it reflects sharp intelligence in dealing with important matters, seeks appetite for freedom and has strong instincts.")
+print("When not in school, I love to:")
+print("* Watch entertainment videos on youtube or facebook")
+print("* Play videogames")
+print("* Pamper my dogs")
+print("I dream of being the owner of one of a famous gaming computer hub in the Philippines.")
